dd479b0e0d493254de9e938b04747710b8a8db89
ryota0624 <tennis.suta6@gmail.com>
2021-04-17 13:29:33 +0900

    add impl

---
+++ src/main/scala/com/github/ryota0624/field/Field.scala
@@ -0,0 +1,21 @@
+package com.github.ryota0624.field
+
+import scala.reflect.macros.whitebox
+
+object Field {
+  def of[E](expr: E): Field[E] = macro FieldOfMacroImpl.fieldOf[E]
+}
+
+case class Field[V](value: V, name: String)
+
+object FieldOfMacroImpl {
+  def fieldOf[E](c: whitebox.Context)(expr: c.Expr[Any]): c.Expr[E] = {
+    import c.universe._
+    import com.github.dwickern.macros.NameOfImpl
+    val nameExpr = NameOfImpl.nameOf(c)(expr)
+    c.Expr(q"""
+         import field_of.Field
+         Field(${expr}, ${nameExpr})
+       """)
+  }
+}
